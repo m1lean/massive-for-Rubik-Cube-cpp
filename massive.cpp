6442330fb12c5beaa75c0279a1645e2d64dcfe8b
Alexander Tsganok <119528928+m1lean@users.noreply.github.com>
2023-02-22 09:45:15 +0200

    Massive

---
+++ massive.cpp
@@ -0,0 +1,8 @@
+char cube[6][3][3] = {
+    { {'W', 'W', 'W'}, {'W', 'W', 'W'}, {'W', 'W', 'W'} }, // White side
+    { {'O', 'O', 'O'}, {'O', 'O', 'O'}, {'O', 'O', 'O'} }, // Orange side
+    { {'G', 'G', 'G'}, {'G', 'G', 'G'}, {'G', 'G', 'G'} }, // Green side
+    { {'R', 'R', 'R'}, {'R', 'R', 'R'}, {'R', 'R', 'R'} }, // Red side
+    { {'B', 'B', 'B'}, {'B', 'B', 'B'}, {'B', 'B', 'B'} }, // Blue side
+    { {'Y', 'Y', 'Y'}, {'Y', 'Y', 'Y'}, {'Y', 'Y', 'Y'} }  // Yellow side
+};
